188c06fa6b70087052bada5dd584cc9263b6047c
Amit Chavda <amitchavda9879@gmail.com>
2022-07-27 18:57:54 +0530

    Initial commit

---
+++ src/component/logo.jsx
@@ -0,0 +1,9 @@
+export default function Logo(){
+    return(
+        <div>
+            <p style={{fontSize:"1.2em"}}>
+                <span style={{color:"#06283D",fontWeight:"bolder"}}>Re</span><span style={{color:"#1363DF",fontWeight:"bolder"}}>Pro</span>
+            </p>
+        </div>
+    );
+}
