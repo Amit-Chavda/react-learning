7a21bea9272021371d9cc992639e4781dc6d4090
Amit Chavda <amitchavda9879@gmail.com>
2022-07-28 17:33:38 +0530

    project structured

--- src/component/logo.jsx
+++
@@ -1,9 +0,0 @@
-export default function Logo(){
-    return(
-        <div>
-            <p style={{fontSize:"1.2em"}}>
-                <span style={{color:"#06283D",fontWeight:"bolder"}}>Re</span><span style={{color:"#1363DF",fontWeight:"bolder"}}>Pro</span>
-            </p>
-        </div>
-    );
-}
