ebadb04227d8a9d70ecad2f349b4cf385b103dad
Amit Chavda <amitchavda9879@gmail.com>
2022-07-31 20:40:42 +0530

    Form ready

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,13 +1,22 @@
 import { useState } from "react";
 export default function Form() {
-    const [formData, setformData] = useState({ firstName: "", lastName: "", email: "", comment: "" });
+    const [formData, setformData] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        comment: "",
+        isFriendly: false,
+        employment: "",
+        favCar: ""
+    });
 
     console.log(formData)
     function handleOnChange(event) {
+        const { name, value, type, checked } = event.target
         setformData((prev) => {
             return {
                 ...prev,
-                [event.target.name]: event.target.value
+                [name]: type === "checkbox" ? checked : value
             }
         })
     }
@@ -20,12 +29,16 @@ export default function Form() {
                 onChange={handleOnChange}
                 name="firstName"
                 value={formData.firstName} />
+            <br />
+            <br />
             <input
                 type="text"
                 placeholder="Last Name"
                 onChange={handleOnChange}
                 name="lastName"
                 value={formData.lastName} />
+            <br />
+            <br />
             <input
                 type="email"
                 placeholder="Email"
@@ -33,10 +46,69 @@ export default function Form() {
                 name="email"
                 value={formData.email} />
 
+            <br />
+            <br />
             <textarea
                 name="comment"
+                placeholder="Comments"
                 onChange={handleOnChange}
                 value={formData.comment} />
+
+            <br />
+            <br />
+            <input
+                type="checkbox"
+                onChange={handleOnChange}
+                name="isFriendly"
+                id="isFriendly"
+                checked={formData.isFriendly} />
+            <label htmlFor="isFriendly">Are you Friendly?</label>
+
+            <fieldset>
+                <legend>
+                    Select emplyment status
+                </legend>
+
+                <input
+                    type="radio"
+                    name="employment"
+                    value="unemployed"
+                    onChange={handleOnChange}
+                    id="unemployed" />
+                <label htmlFor="unemployed">Unemployed</label>
+
+                <input
+                    type="radio"
+                    name="employment"
+                    value="part-time"
+                    onChange={handleOnChange}
+                    id="part-time" />
+                <label htmlFor="part-time">Part-time</label>
+
+                <input
+                    type="radio"
+                    name="employment"
+                    value="full-time"
+                    onChange={handleOnChange}
+                    id="full-time" />
+                <label htmlFor="full-time">Full-time</label>
+
+            </fieldset>
+
+            <br />
+            <label htmlFor="cars">Which is your favourite car:</label>
+            <select
+                name="favCar"
+                id="cars"
+                onChange={handleOnChange}
+                value={formData.favCar}
+            >
+                <option value="">--Choose--</option>
+                <option value="volvo">Volvo</option>
+                <option value="saab">Saab</option>
+                <option value="mercedes">Mercedes</option>
+                <option value="audi">Audi</option>
+            </select>
         </>
     )
 }
