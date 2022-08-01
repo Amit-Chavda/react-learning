a6709a3ecf144023dcd44b54620c6780f7b3d8f6
Amit Chavda <amitchavda9879@gmail.com>
2022-08-01 09:38:49 +0530

    useEffect example

--- src/component/App.js
+++ src/component/App.js
@@ -1,10 +1,24 @@
 import '../styles/App.css';
-import Login from './Login';
+import { useState, useEffect } from "react";
+import axios from "axios"
 
 function App() {
+
+  const [starWarsData, setStarWarsData] = useState({});
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/people/1")
+      .then((res) => {
+        setStarWarsData(res.data)
+        console.log(starWarsData)
+      })
+  }, [count]);
+
   return (
     <div className="App">
-      <Login />
+      <h2>Counter:{count}</h2>
+      <button onClick={()=>setCount(prev=>prev+1)}>Add</button>
     </div>
   );
 }
