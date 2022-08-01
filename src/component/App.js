7b3dbf5135960deee89bedb149bd78d9c9abf416
Amit Chavda <amitchavda9879@gmail.com>
2022-08-01 10:32:01 +0530

    Updated App.js

--- src/component/App.js
+++ src/component/App.js
@@ -5,20 +5,20 @@ import axios from "axios"
 function App() {
 
   const [starWarsData, setStarWarsData] = useState({});
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1);
 
   useEffect(() => {
-    axios.get("https://swapi.dev/api/people/1")
+    axios.get("https://swapi.dev/api/people/"+count)
       .then((res) => {
         setStarWarsData(res.data)
         console.log(starWarsData)
       })
   }, [count]);
-
   return (
     <div className="App">
       <h2>Counter:{count}</h2>
-      <button onClick={()=>setCount(prev=>prev+1)}>Add</button>
+      <h1>{JSON.stringify(starWarsData)}</h1>
+      <button onClick={() => setCount(prev => prev + 1)}>Generate Next Character</button>
     </div>
   );
 }
